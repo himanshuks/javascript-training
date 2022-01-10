b3a2db0fa8f572f5cbf9da219ab3fcdd25a4fb98
Himanshu Kumar Singh <himanshu.kr5294@gmail.com>
2022-01-10 10:58:33 +0530

    undefined in javascript

--- variableEnvironment.js
+++ variableEnvironment.js
@@ -15,3 +15,25 @@ function b() {
   var z = 100;
   console.log("z in B", z);
 }
+
+// Undefined is like a placeholder provided to variables when memory is allocated in 1st phase of execution context
+
+console.log(x);
+
+// So here it will be equals to undefined
+
+if (x === undefined) {
+  console.log("x is undefined");
+} else {
+  console.log("x in not undefined");
+}
+
+var x = 5;
+
+// Here not, as it's been assigned with 5
+
+if (x === undefined) {
+  console.log("x is undefined");
+} else {
+  console.log("x in not undefined");
+}

03b358c8fe0e7ec910f422c5463518ec0fd69104
Himanshu Kumar Singh <himanshu.kr5294@gmail.com>
2022-01-26 10:26:17 +0530

    first class citizens in javascript

--- javascriptJargons.js
+++ javascriptJargons.js
@@ -20,3 +20,29 @@ c();
 var z = function () {
   console.log("AF called");
 };
+
+// Named function expression - function having name and used as value (function expression)
+var y = function xyz() {
+  console.log("NFX called");
+  console.log(xyz);
+};
+
+z();
+// xyz(); // Will give error -> XYZ is not defined because XYZ is defined in function's local scope and can't be outside in global scope
+
+function display(param1, param2) {
+  // These are parameters
+  console.log("The name is", param1, param2);
+}
+
+display("HKS", 10); // These are arguments
+
+// First Class Function - Ability to pass function as value into another function as arguments which can return functions too
+// Also know as First Class Citizens
+var abc = function items() {
+  return function () {
+    console.log("This is retuned function");
+  };
+};
+
+console.log(`abc Function`, abc());

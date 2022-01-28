843db8d4a77d8bc6950dde424b54f1ba0a1e289a
Himanshu Kumar Singh <himanshu.kr5294@gmail.com>
2022-01-28 15:56:10 +0530

    apply method in javascript

--- callApplyBind.js
+++ callApplyBind.js
@@ -29,3 +29,35 @@ let nameTwo = {
 
 nameOne.printName.call(nameTwo); // undefined is coming as no SECOND argument is provided
 nameOne.printName.call(nameTwo, "Delhi");
+
+// Generally, the function is put outside the object for easy use
+
+let printNameOutside = function (homeTown, state) {
+  console.log(
+    "Name is",
+    this.firstName +
+      " " +
+      this.lastName +
+      " lives in " +
+      homeTown +
+      " of state " +
+      state
+  );
+};
+
+let nameThree = {
+  firstName: "Rohit",
+  lastName: "Sharma",
+};
+
+printNameOutside.call(nameThree); // undefined is coming as no SECOND, THIRD argument are provided
+printNameOutside.call(nameThree, "Mumbai"); // undefined is coming as no THIRD argument is provided
+printNameOutside.call(nameThree, "Mumbai", "Maharashtra");
+
+// APPLY - works almost similar to the CALL as explained above
+// Only difference is that in APPLY, we pass additional arguments in ARRAY form
+
+// printNameOutside.apply(nameThree, "Mumbai", "Maharashtra"); // TypeError: CreateListFromArrayLike called on non-object
+printNameOutside.apply(nameThree); // undefined is coming as no SECOND, THIRD argument are provided
+printNameOutside.apply(nameThree, ["Mumbai"]); // undefined is coming as no THIRD argument is provided
+printNameOutside.apply(nameThree, ["Mumbai", "Maharashtra"]);

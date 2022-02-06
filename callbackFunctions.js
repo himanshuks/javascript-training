6a0699692f44827bcb2b170e71213e5e9698e67f
Himanshu Kumar Singh <himanshu.kr5294@gmail.com>
2022-02-06 19:30:30 +0530

    callback function on event listeners

--- callbackFunctions.js
+++ callbackFunctions.js
@@ -11,3 +11,21 @@ function a(z) {
 a(function b() {
   console.log("This is callback function");
 });
+
+// When we call a function A and pass another function B as argument, B is called callback function
+// Since we have passed B as parameter to A, B will be called sometime later as per logic of A, hence know as callback function
+
+setTimeout(function () {
+  console.log("Timer executed after 3s");
+}, 3000);
+
+function getCount() {
+  let count = 0;
+  document.getElementById("clickMe").addEventListener("click", function xyz() {
+    console.log("Button is clicked", ++count);
+  });
+}
+
+getCount();
+
+// DOM API - these are also call back functions which are present in Web APi environment, and once the action is triggered, function is called

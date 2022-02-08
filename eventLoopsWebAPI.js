758ad3260a50a1eccac37ae6c590d13edbb5707f
Himanshu Kumar Singh <himanshu.kr5294@gmail.com>
2022-02-08 13:06:26 +0530

    microtask and callback queue in javascript

--- eventLoopsWebAPI.js
+++ eventLoopsWebAPI.js
@@ -15,3 +15,24 @@ console.log("End");
 // Simultaneously, timer is started with countdown provided in callback function
 // Global Execution gets popped out from call stack after all JS work
 // When timer expires, XYZ moves into the CALLBACK queue
+
+// EVENTLOOP monitors the CALLBACK queue and puts the XYZ into the JS call stack
+// EVENTLOOP puts call function from CALLBACK queue only when JS call stack is empty
+// That's why -> setTimeout(function(){},0) -> Executes at last as it's waiting for JS call stack to get empty
+
+console.log("Big start");
+
+setTimeout(function cbT() {
+  console.log("Timer 2 called");
+}, 5000);
+
+fetch("https://api.netflix.com").then(function cbF() {
+  console.log("Netflix called");
+});
+
+console.log("Big end");
+
+// MicroTask queue - Similar to CALLBACK queue but has HIGHER priority
+// Call back functions from PROMISES and Mutation Observer
+
+// When tasks are present both in MicroTask and CALLBACK queue, EVENTLOOP executes all functions present in MicroTask first when JS call stack is empty. Then CALLBACK queue's functions are executed
